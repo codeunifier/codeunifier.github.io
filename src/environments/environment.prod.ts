c806ec077df08a8aaa5dfc01e97467d1eeb9f7e9
CodeUnifier <codeunifier@gmail.com>
2025-10-22 10:45:15 -0700

    stripping sensitive secrets from prod deploy and switching to user-provided keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   jiraInstance: '__JIRA_INSTANCE_PLACEHOLDER__',
-  jiraApiToken: '__API_KEY_PLACEHOLDER__',
-  jiraAccountEmail: '__EMAIL_PLACEHOLDER__',
+
+  // Require the user to enter these on prod for security reasons
+  jiraApiToken: '',
+  jiraAccountEmail: '',
 };
