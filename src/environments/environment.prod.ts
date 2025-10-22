2a3f7391562c52562b90f40f5c17cd016861d82b
CodeUnifier <codeunifier@gmail.com>
2025-10-22 10:55:52 -0700

    updating api calls to not proxy in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   jiraInstance: '__JIRA_INSTANCE_PLACEHOLDER__',
+  jiraBaseUrl: 'https://__JIRA_INSTANCE_PLACEHOLDER__/rest/api/3',
 
   // Require the user to enter these on prod for security reasons
   jiraApiToken: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   jiraInstance: 'YOUR_JIRA_INSTANCE_HERE',
+  jiraBaseUrl: '/api/jira/rest/api/3',
   jiraApiToken: 'YOUR_API_TOKEN_HERE',
   jiraAccountEmail: 'YOUR_EMAIL_HERE',
 };
